69e574f364ae245f8649da06739a3c8c844efe59
Tomasz Świstak <swistak.tomasz@outlook.com>
2021-09-19 20:31:19 +0200

    removed not used code

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,9 +1,7 @@
 const lexicographic = require("./lexicographic");
 const plainChanges = require("./plainChanges");
-const cyclicShifts = require("./cyclicShifts");
 
 module.exports = {
   lexicographic,
   plainChanges,
-  cyclicShifts,
 };
